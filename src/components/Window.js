627ab40be47bff2ed8c815ecd8e63b636685424f
Brittany Fenney <brittanyfenney@gmail.com>
2021-03-10 19:11:25 -0700

    refactored to functional components

--- src/components/Window.js
+++ src/components/Window.js
@@ -1,14 +1,13 @@
-import React from 'react';
+import React from "react";
+import "98.css";
+import "../index.css";
 
-export default  class Window extends React.Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-
-    return (
-      <h1>Hello</h1>
-    )
-  }
+export default function Window() {
+  return (
+    <div class-name="main-window">
+      <p>Hello. Are you new here?</p>
+      <button>Yes.</button>
+      <button>No.</button>
+    </div>
+  );
 }
