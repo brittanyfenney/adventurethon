1248bab5634b11e1198a095df695bbc07f16ce21
Brittany Fenney <brittanyfenney@gmail.com>
2021-03-10 18:30:30 -0700

    display main window

--- src/App.js
+++ src/App.js
@@ -1,47 +1,33 @@
-import React, { useState } from 'react';
+import "98.css"
+import "./index.css"
+import Window from './Window'
 
-import { fetchWeather } from './api/fetchWeather'
-import './App.css'
-
-const App = () => {
-  const [query, setQuery] = useState('')
-  const [weather, setWeather] = useState({});
-
-  const search = async (e) => {
-    if(e.key === 'Enter') {
-      const data = await fetchWeather(query)
-
-      setWeather(data);
-      setQuery('');
-    }
-  }
-  return(
-    <div className="main-container">
-      <input
-      type="text"
-      className="search"
-      placeholder="search..."
-      value={query}
-      onChange={(e) => setQuery(e.target.value)}
-      onKeyPress={search}
-      />
-    {weather.main && (
-      <div className="city">
-        <h2 className="city-name">
-          <span>{weather.name}</span>
-          <sup>{weather.sys.country}</sup>
-        </h2>
-        <div className="city-temp">
-          {Math.round(weather.main.temp)}<sup>&deg;C</sup>
-          </div>
-          <div className="info">
-          <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
-          <p>{weather.weather[0].description}</p>
-          </div>
+function App() {
+  return (
+    <div className="App">
+      <div className="window">
+      <div className="title-bar">
+        <div className="title-bar-text">Adventurethon</div>
+        <div className="title-bar-controls">
+          <button aria-label="Minimize" />
+          <button aria-label="Maximize" />
+          <button aria-label="Close" />
+        </div>
       </div>
-    )}
+
+      <div className="window-body">
+        {/* <p style={{ textAlign: "center" }}>Current count: 5</p>
+        <div className="field-row" style={{ justifyContent: "center" }}>
+          <button>+</button>
+          <button>-</button>
+          <button>0</button> */}
+          <Window />
+        {/* </div> */}
+      </div>
     </div>
-  )
+
+    </div>
+  );
 }
 
-export default App;
+export default App
---
+++ src/Window.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default  class Window extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+
+    return (
+      <h1>Hello</h1>
+    )
+  }
+}
