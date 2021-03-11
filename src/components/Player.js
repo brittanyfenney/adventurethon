9f5f5161b20fba20ff8df571c298fcc4e6836dd1
Brittany Fenney <brittanyfenney@gmail.com>
2021-03-11 15:20:02 -0700

    “net”

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,14 +1,9 @@
 // import React, { useState, useEffect } from "react";
-// import YouTube from "react-youtube";
+import YouTube from "react-youtube";
 
 export function Player() {
   console.log("PLAYER FUNCTION");
 
-  // const _onReady = (event) => {
-  //   console.log("_onReady");
-  //   // cElement = event;
-  //   // toggleReady();
-  // };
 
   return (
     <div>
@@ -18,8 +13,8 @@ export function Player() {
         </button>
 
       <button type="button">Next</button>
-      <iframe title ="player" src="https://www.youtube.com/embed/qq09UkPRdFY?autoplay=1" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
-      {/* <YouTube videoId={"qq09UkPRdFY"} onReady={_onReady}/> */}
+      <YouTube videoId={"qq09UkPRdFY"}/>
+      <button type="button">Another button</button>
     </div>
   );
 }
