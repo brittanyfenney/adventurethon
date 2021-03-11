39ada6a8bc9e03ea636c061417ebef27f304750a
Brittany Fenney <brittanyfenney@gmail.com>
2021-03-11 16:25:09 -0700

    “net”

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,23 +1,29 @@
 // import React, { useState, useEffect } from "react";
-import React from 'react'
+import React from "react";
 import YouTube from "react-youtube";
 
 export class Player extends React.Component {
+  constructor() {
+    super();
+    this.onReady = this.onReady.bind(this);
+  }
+
+  onReady(event) {
+    console.log("_onReady");
+    // cElement = event;
+    // toggleReady();
+  }
+
   render() {
-  console.log("PLAYER FUNCTION");
+    console.log("PLAYER FUNCTION");
+    return (
+      <div>
+        <button type="button">Play</button>
 
-
-  return (
-    <div>
-
-        <button type="button">
-          Play
-        </button>
-
-      <button type="button">Next</button>
-      <YouTube videoId={"qq09UkPRdFY"}/>
-      <button type="button">Another button</button>
-    </div>
-  )
+        <button type="button">Next</button>
+        <YouTube videoId={"qq09UkPRdFY"} onReady={this.onReady} />
+        <button type="button">Another button</button>
+      </div>
+    );
   }
 }
