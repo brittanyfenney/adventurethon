f28b0a2e1dd7bad3d030951d0256e57927a9e9e2
Brittany Fenney <brittanyfenney@gmail.com>
2021-03-13 21:23:40 -0700

    rearrange buttons

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,61 +1,70 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { Item } from './Item'
+import { Item } from "./Item";
 import "../index.css";
 
 export function Room(props) {
   const room = useSelector((state) => state.rooms[props.match.params.id]);
-  const togglePlayer = props.togglePlayer
+  const togglePlayer = props.togglePlayer;
   const inventory = useSelector((state) => state.inventory);
-  console.log(inventory)
-
+  console.log(inventory);
 
   if (room.name) {
     return (
       <div className="nes-container is-dark with-title" id="room-window">
         <div>
+          <p>You are in the {room.name}.</p>
+          <p>What would you like to do?</p>
 
-        <p>You are in the {room.name}.</p>
-        <p>What would you like to do?</p>
-
-        <div className="direction-buttons">
-          {room.directions.east && (
-            <Link to={`/room/${room.directions.east}`}>
-              <button className="nes-btn">Go East</button><br />
-            </Link>
-          )}
-          {room.directions.west && (
-            <Link to={`/room/${room.directions.west}`}>
-              <button className="nes-btn">Go West</button><br />
-            </Link>
-          )}
           {room.directions.north && (
             <Link to={`/room/${room.directions.north}`}>
-              <button className="nes-btn">Go North</button><br />
+              <button className="nes-btn">Go North</button>
+              <br />
             </Link>
           )}
           {room.directions.south && (
             <Link to={`/room/${room.directions.south}`}>
-              <button className="nes-btn">Go South</button><br />
+              <button className="nes-btn">Go South</button>
+              <br />
             </Link>
           )}
-          {room.directions.upstairs && (
-            <Link to={`/room/${room.directions.upstairs}`}>
-              <button className="nes-btn">Go Upstairs</button><br />
-            </Link>
-          )}
-          {room.directions.downstairs && (
-            <Link to={`/room/${room.directions.downstairs}`}>
-              <button className="nes-btn">Go Downstairs</button><br />
-            </Link>
-          )}
-          {(room.directions.hidden && (4 in inventory)) ? (
-            <Link to={`/room/${room.directions.hidden}`}>
-              <button className="nes-btn">A 3D door!</button><br />
-            </Link>
-          ) : <div></div>}
-        </div>
+
+          <div className="direction-buttons">
+            {room.directions.east && (
+              <Link to={`/room/${room.directions.east}`}>
+                <button className="nes-btn">Go East</button>
+                <br />
+              </Link>
+            )}
+            {room.directions.west && (
+              <Link to={`/room/${room.directions.west}`}>
+                <button className="nes-btn">Go West</button>
+                <br />
+              </Link>
+            )}
+
+            {room.directions.upstairs && (
+              <Link to={`/room/${room.directions.upstairs}`}>
+                <button className="nes-btn">Go Upstairs</button>
+                <br />
+              </Link>
+            )}
+            {room.directions.downstairs && (
+              <Link to={`/room/${room.directions.downstairs}`}>
+                <button className="nes-btn">Go Downstairs</button>
+                <br />
+              </Link>
+            )}
+            {room.directions.hidden && 4 in inventory ? (
+              <Link to={`/room/${room.directions.hidden}`}>
+                <button className="nes-btn">A 3D door!</button>
+                <br />
+              </Link>
+            ) : (
+              <div></div>
+            )}
+          </div>
         </div>
 
         <Item room={room} togglePlayer={togglePlayer} />
